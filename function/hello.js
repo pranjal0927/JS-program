5da5e2055c99ffd354492f1f740674ea720e835e
pranjal0927 <pranjal0927@gmail.com>
2024-05-01 22:50:43 +0530

    function syntax

---
+++ function/hello.js
@@ -0,0 +1,21 @@
+// function declaration
+
+function printMyName(){
+    console.log('hello')
+}
+printMyName()
+
+// function expression
+
+let name = function(){
+
+    console.log('pranjal');
+}
+name()
+
+//arrow funtion
+
+let surName = ( ) =>{
+    console.log('borole')
+}
+surName()

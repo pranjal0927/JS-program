8fc6d3a335196b417d8450782a4b3ffe622e91e1
pranjal0927 <pranjal0927@gmail.com>
2024-05-02 18:29:38 +0530

    add sub mult div using choice input

---
+++ function/test1.js
@@ -0,0 +1,57 @@
+
+
+function choice(){
+    let input = +prompt("enter the choice")
+
+    console.log('choice :', input);
+    if( input == 5)
+    {
+        console.log('thank you for the Choice')
+        return;
+    }
+    
+    let num1 = +prompt('enter number num1')
+    let num2 = +prompt('enter number num2')
+
+    console.log('first number is:',num1);
+    console.log('Second number is:',num2);
+    
+
+    switch (input){
+        case 1:
+
+        function Add (num1 , num2)
+        {
+            console.log('Addition of number1 and number2 :', num1 + num2 )
+        }
+        Add(num1, num2 )
+        break
+       
+        case 2:
+    
+        function Sub (num1 , num2)
+        {
+            console.log('substraction of number1 and number2 :', num1 - num2 )
+        }
+        Sub(num1, num2 )
+        break
+
+        case 3:
+    
+        function Multi (num1 , num2)
+        {
+            console.log('Multiplication of number1 and number2 :', num1 * num2 )
+        }
+        Multi(num1, num2 )
+        break            
+
+        case 4:
+    
+        function Division (num1 , num2)
+        {
+            console.log('Division of number1 and number2 :', num1 / num2 )
+        }
+        Division(num1, num2 )
+        break
+   }
+}choice()

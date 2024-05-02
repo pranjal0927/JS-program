060accbe83c7dfc970a804943b0accc5b49f443f
pranjal0927 <pranjal0927@gmail.com>
2024-05-02 22:26:53 +0530

    * pattern program

---
+++ pattern/pattern1.js
@@ -0,0 +1,73 @@
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+// let str = ""
+// for (let i = 1; i <= 4; i++)
+// {
+//     for(let j = 1; j <= 4; j++)
+//     {
+//         str += "* "
+//     }
+//     str += "\n"
+// }
+// console.log(str)
+
+
+// *
+// **
+// ***
+// ****
+
+// let str = ""
+
+// for(let i = 1; i <= 4; i++)
+// {
+//     for(let j = 1; j <= i; j++)
+//     {
+//         str +='*'
+//     }
+//     str += '\n'    
+// }
+// console.log(str)
+
+
+// 2345
+// 3456
+// 4567
+// 5678
+
+// let str = ''
+
+// for(let i str= 1; i <= 4; i++)
+// {
+//     for(let j = 1; j <= 4; j++)
+//     {
+//         str += i + j
+//     }
+//     str += "\n"
+// }
+// console.log(str)
+
+
+// ****
+// ***
+// **
+// *
+
+// let str = ""
+
+// for(let i = 1; i <= 4; i++)
+// {
+//     for(let j = 4; j >= i; j--)
+//     {
+//         str += '*'
+//     }
+//     str += '\n'
+// }
+// console.log(str)
+
+
+
+

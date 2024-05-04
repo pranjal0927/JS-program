7a5ef605bf2163c0c87b98458ec5ab5d50b202ee
pranjal0927 <pranjal0927@gmail.com>
2024-05-04 22:43:57 +0530

    dialog box

---
+++ basics/dialogbox.js
@@ -0,0 +1,7 @@
+// const alertResult = alert ("your messages goes here")           //typeof undefined
+
+// const isConfirmed = confirm ("your messages goes here")         // type of boolean return true or false value
+
+// const userName = prompt(`please enter your name`)
+
+// let UserInput = +prompt('enter number')                            // + convert string into number 

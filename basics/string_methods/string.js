f9f1b1fdf0464f353bc1b54bb4a0d5b38c39c275
pranjal0927 <pranjal0927@gmail.com>
2024-05-05 19:12:19 +0530

    string method

---
+++ basics/string_methods/string.js
@@ -0,0 +1,14 @@
+const name = `pranjal`
+let surName = `BOROLE`
+
+let capitalName = name.toUpperCase()
+let lowerSurName= surName.toLowerCase()
+
+let dummyText = `         hi how are you?       `
+
+let dummyTextTrim = dummyText.trim()            //delete empty space from start and end
+
+let dummyTextIncludes = dummyText.includes()    //check if the string content letter and space mention 
+
+
+

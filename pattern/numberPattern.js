319dc837dbccdcaa12e1bf76fc8670524fbda87d
pranjal0927 <pranjal0927@gmail.com>
2024-05-08 18:17:05 +0530

    updated 1 more pattern

--- pattern/numberPattern.js
+++ pattern/numberPattern.js
@@ -78,8 +78,64 @@
 
 
 
-let res = ''
-for(let i = 1; i<= 5;i++)
+// 1
+// 21
+// 321
+// 4321
+// 54321
+// 4321
+// 321
+// 21
+// 1
+
+
+// let res = ''
+
+// for(let i = 1; i <= 5; i++)
+//     {
+//         for(let j = i; j>= 1; j--)
+//             {
+//                 res += j
+//             }
+
+//             res +=`\n`
+
+//         }
+
+//         for(let i = 4; i>=1;i--)
+//     {
+//         for(let k = i;k>=1;k--)
+//             {
+//                 res += k
+//             }
+//             res +=`\n`
+//     }                    
+//     console.log(res)
+
+
+
+// 54321
+//  5432
+//   543
+//    54
+//     5
+
+
+
+let res= ''
+
+for(let i =1; i<= 5;i++ )
     {
-        for(let j = )
-    }
+        for(let j = 1;j < i;j++)
+            {
+                res +=" "
+
+            }
+           
+        for(let k = 5; k >= i;k--)
+            {
+                res += k
+            }
+            res +=`\n`
+        }
+    console.log(res)

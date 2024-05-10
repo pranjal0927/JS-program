50c265eed6e0b318cecf954aeb8a6e799b35930c
pranjal0927 <pranjal0927@gmail.com>
2024-05-10 19:00:27 +0530

    fahrenheit to celsius

---
+++ if_statement/f2c.js
@@ -0,0 +1,14 @@
+// fahrenheit to celsius 
+
+
+// let num = +prompt(`enter number`)
+let num = 108
+let res = 0;
+console.log("fahrenheit :", num)
+
+if(num!=0)
+    {
+        res = (num -32) *5/9
+    }
+
+console.log(res)
